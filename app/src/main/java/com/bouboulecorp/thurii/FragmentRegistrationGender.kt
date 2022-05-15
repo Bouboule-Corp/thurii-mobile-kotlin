ba721e4ee549a04cf0ab9de503ae33ca4bde3852
clara <yuhui.xu@epitech.eu>
2022-05-15 21:30:04 +0200

    merge branch13 to branch14

---
+++ app/src/main/java/com/bouboulecorp/thurii/FragmentRegistrationGender.kt
@@ -0,0 +1,6 @@
+package com.bouboulecorp.thurii
+
+import androidx.fragment.app.Fragment
+
+class FragmentRegistrationGender : Fragment(R.layout.registration_gender) {
+}
---
+++ app/src/main/java/com/bouboulecorp/thurii/RegistrationActivity.kt
@@ -0,0 +1,61 @@
+package com.bouboulecorp.thurii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.bouboulecorp.thurii.databinding.ActivityMainBinding
+import com.bouboulecorp.thurii.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegistrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val homeFragment = FragmentRegistrationGender()
+        val fragment : Fragment? =
+            supportFragmentManager.findFragmentByTag(FragmentRegistrationGender::class.java.simpleName)
+
+        if (fragment !is FragmentRegistrationGender) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, homeFragment, FragmentRegistrationGender::class.java.simpleName)
+                .commit()
+        }
+        val con_button = findViewById<Button>(R.id.continueBtn)
+        val btn_female = findViewById<ImageButton>(R.id.btn_female)
+        val btn_male = findViewById<ImageButton>(R.id.btn_male)
+        val progress = findViewById<ProgressBar>(R.id.step_bar)
+
+        btn_female.setOnClickListener {
+            btn_female.setAlpha(1.0f)
+            btn_male.setAlpha(0.4f)
+            con_button.setBackgroundColor(ContextCompat.getColor(this, R.color.color_logo));
+        }
+
+        btn_male.setOnClickListener {
+            btn_male.setAlpha(1.0f)
+            btn_female.setAlpha(0.4f)
+            con_button.setBackgroundColor(ContextCompat.getColor(this, R.color.color_logo));
+        }
+        con_button.setOnClickListener {
+            //button.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            var i = progress.progress
+            progress.setProgress(i + 25)
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+}
