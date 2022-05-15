60fd27a4bf6fd6475b362896ce37ebfcb9df2893
clara <yuhui.xu@epitech.eu>
2022-05-15 21:30:04 +0200

    going to try fragments in branch13

--- app/src/main/java/com/bouboulecorp/thurii/MainActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/MainActivity.kt
@@ -9,28 +9,6 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registration_gender)
-
-        val con_button = findViewById<Button>(R.id.continue_button)
-        val btn_female = findViewById<ImageButton>(R.id.btn_female)
-        val btn_male = findViewById<ImageButton>(R.id.btn_male)
-        val progress = findViewById<ProgressBar>(R.id.step_bar)
-
-        btn_female.setOnClickListener {
-            btn_female.setAlpha(1.0f)
-            btn_male.setAlpha(0.4f)
-            con_button.setBackgroundColor(ContextCompat.getColor(this, R.color.color_logo));
-        }
-
-        btn_male.setOnClickListener {
-            btn_male.setAlpha(1.0f)
-            btn_female.setAlpha(0.4f)
-            con_button.setBackgroundColor(ContextCompat.getColor(this, R.color.color_logo));
-        }
-        con_button.setOnClickListener {
-            //button.setBackgroundColor(Color.parseColor("#FFBB86FC"))
-            var i = progress.progress
-            progress.setProgress(i + 25)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
