d0b9c17ee074a0db5f57d6355135c071eed78275
lucas <lucas.arroyo70@gmail.com>
2022-06-05 14:12:42 +0200

    finish LogIn SignIn menu

---
+++ app/src/main/java/com/bouboulecorp/thurii/activities/LogIn/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.bouboulecorp.thurii.activities.LogIn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bouboulecorp.thurii.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
@@ -0,0 +1,32 @@
+package com.bouboulecorp.thurii.activities.LogInSignInMenu
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.TextView
+import com.bouboulecorp.thurii.R
+import com.bouboulecorp.thurii.activities.LogIn.LoginActivity
+
+class LogInSignInMenuActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_sign_in_menu)
+
+        // Declaring and Initializing
+        // the TextView from the layout file
+        val mTextView = findViewById<TextView>(R.id.textView)
+
+        // Finding and displaying the content
+        // that consists a URL as a hyperlink
+        mTextView.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    fun goToLoginPage(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
