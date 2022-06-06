6046e92b093a333093449adddd10a6fc486b7f80
clara <yuhui.xu@epitech.eu>
2022-06-06 21:27:59 +0200

    continue button active after choosed values

--- app/src/main/java/com/bouboulecorp/thurii/FragmentRegistrationGender.kt
+++ app/src/main/java/com/bouboulecorp/thurii/FragmentRegistrationGender.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class FragmentRegistrationGender : Fragment(R.layout.registration_gender) {
@@ -23,14 +25,20 @@ class FragmentRegistrationGender : Fragment(R.layout.registration_gender) {
     ): View? {
         val lay = super.onCreateView(inflater, container, savedInstanceState)
 
+        val continue_btn = getActivity()?.findViewById<Button>(R.id.continueBtn)
+
         val btn_female = lay!!.findViewById<ImageButton>(R.id.btn_female)
         val btn_male = lay!!.findViewById<ImageButton>(R.id.btn_male)
 
+        continue_btn?.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.custom_color_slider_widget_unselected))
+
         btn_female.setOnClickListener {
+            continue_btn?.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.custom_color_primary_))
             btn_female.setAlpha(1.0f)
             btn_male.setAlpha(0.4f)
         }
         btn_male.setOnClickListener {
+            continue_btn?.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.custom_color_primary_))
             btn_male.setAlpha(1.0f)
             btn_female.setAlpha(0.4f)
         }
