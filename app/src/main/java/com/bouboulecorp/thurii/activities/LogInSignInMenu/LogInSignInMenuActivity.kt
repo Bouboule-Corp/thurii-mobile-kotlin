5bbb6497106fadfcee0c3fd99d3643238965e712
Neyross <74605059+Neyross@users.noreply.github.com>
2022-06-07 11:39:30 +0200

    Update LogInSignInMenuActivity.kt

--- app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
@@ -1,4 +1,4 @@
-package com.bouboulecorp.thurii.activities.LogInSignInMenu
+// package com.bouboulecorp.thurii.activities.LogInSignInMenu
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bouboulecorp.thurii.activities.LogIn.LoginActivity
 import com.bouboulecorp.thurii.R
+
 class LogInSignInMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
