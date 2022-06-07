3e891c781eb19d7522f7da065e82ff9c6dbaf8a9
Neyross <74605059+Neyross@users.noreply.github.com>
2022-06-07 11:42:15 +0200

    Update LogInSignInMenuActivity.kt

--- app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
@@ -1,4 +1,4 @@
-// package com.bouboulecorp.thurii.activities.LogInSignInMenu
+package com.bouboulecorp.thurii.activities.LogInSignInMenu
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
