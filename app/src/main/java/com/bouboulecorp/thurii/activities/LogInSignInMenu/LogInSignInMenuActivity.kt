8d5ea58a2fb2bc2bce717c42ead2aba38480b03d
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-07 10:49:00 +0200

    Update LogInSignInMenuActivity.kt

--- app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/activities/LogInSignInMenu/LogInSignInMenuActivity.kt
@@ -1,10 +1,10 @@
 package com.bouboulecorp.thurii.activities.LogInSignInMenu
+import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.bouboulecorp.thurii.activities.LogIn.LoginActivity
 import com.bouboulecorp.thurii.R
 
