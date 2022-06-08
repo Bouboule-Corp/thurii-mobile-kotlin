4ac59e8eb6bf1e9b46a51945d18b5d7bb14eac57
yuhui <yuhui.xu@nuabee.fr>
2022-06-08 15:53:36 +0200

    added fragment test

--- app/src/androidTest/java/com/bouboulecorp/thurii/FragmentRegistrationGenderTest.kt
+++ app/src/androidTest/java/com/bouboulecorp/thurii/FragmentRegistrationGenderTest.kt
@@ -1,10 +1,11 @@
 package com.bouboulecorp.thurii
 
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import junit.framework.TestCase
 import org.junit.Test
@@ -15,7 +16,33 @@ class FragmentRegistrationGenderTest{
 
     @Test
     fun test_isActivityInView() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.fragmentContainer)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_visibility_title() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.gender_title))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.gender_des))
+            .check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun test_isFragmentTitleCorrect() {
+        launchFragmentInContainer<FragmentRegistrationGender>(
+            fragmentArgs = null
+        )
+        onView(withId(R.id.gender_title)).check(matches(withText(R.string.register_gender_title)))
+    }
+
+    @Test
+    fun test_isFragmentDesCorrect() {
+        launchFragmentInContainer<FragmentRegistrationGender>(
+            fragmentArgs = null
+        )
+        onView(withId(R.id.gender_des)).check(matches(withText(R.string.register_first_des)))
     }
 }
