10856774023019ec6f82b213b8cc6b28009ef5ac
yuhui <yuhui.xu@nuabee.fr>
2022-06-09 16:47:05 +0200

    added directory for test, changed activity place

--- app/src/androidTest/java/com/bouboulecorp/thurii/RegistrationTests/FragmentRegistrationGenderTest.kt
+++ app/src/androidTest/java/com/bouboulecorp/thurii/RegistrationTests/FragmentRegistrationGenderTest.kt
@@ -1,4 +1,4 @@
-package com.bouboulecorp.thurii
+package com.bouboulecorp.thurii.RegistrationTests
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.core.app.ActivityScenario
@@ -8,6 +8,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.bouboulecorp.thurii.MainActivity
+import com.bouboulecorp.thurii.R
 import com.bouboulecorp.thurii.Registration.RegistrationFragment.FragmentRegistrationGender
 import org.junit.Test
 import org.junit.runner.RunWith
