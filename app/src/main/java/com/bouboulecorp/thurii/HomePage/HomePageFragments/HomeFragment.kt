96441fdf014733502a5d97599b4032c28badfd76
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-12 23:36:44 +0200

    Update HomeFragment.kt

--- app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/HomeFragment.kt
+++ app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/HomeFragment.kt
@@ -30,7 +30,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, 
+        inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
