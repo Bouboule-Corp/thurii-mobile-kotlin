7e47c5ed24527c9eb6d22252335231dee947f885
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-12 12:07:01 +0200

    Update LogInSignInMenuTests.kt

--- app/src/androidTest/java/com/bouboulecorp/thurii/LogInSignInMenu/LogInSignInMenuTests.kt
+++ app/src/androidTest/java/com/bouboulecorp/thurii/LogInSignInMenu/LogInSignInMenuTests.kt
@@ -1,14 +1,11 @@
 package com.bouboulecorp.thurii.LogInSignInMenu
-
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.bouboulecorp.thurii.LogIn.LoginActivity
 import com.bouboulecorp.thurii.R
 import org.junit.Test
 import org.junit.runner.RunWith
