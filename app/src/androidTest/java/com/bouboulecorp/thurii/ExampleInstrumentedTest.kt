e0b79d97687142c70eaf32f9bebb1acb0bd01d4c
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-12 12:06:09 +0200

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/bouboulecorp/thurii/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bouboulecorp/thurii/ExampleInstrumentedTest.kt
@@ -1,16 +1,9 @@
 package com.bouboulecorp.thurii
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert.*
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
