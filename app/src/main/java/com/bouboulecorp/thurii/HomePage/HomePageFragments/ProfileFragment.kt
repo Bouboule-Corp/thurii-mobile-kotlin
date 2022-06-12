4c04a4be94758074c6bb05ce590ed0a696113fd5
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-12 23:37:55 +0200

    Update ProfileFragment.kt

--- app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/ProfileFragment.kt
+++ app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/ProfileFragment.kt
@@ -30,7 +30,7 @@ class ProfileFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, 
+        inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
