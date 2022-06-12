4b90748bc2924eaed8bd4caba87237c7bf371c58
clara <yuhui.xu@epitech.eu>
2022-06-12 19:10:39 +0200

    added double auth page

--- app/src/main/java/com/bouboulecorp/thurii/DoubleAuth/DoubleAuthActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/DoubleAuth/DoubleAuthActivity.kt
@@ -1,6 +1,8 @@
 package com.bouboulecorp.thurii.DoubleAuth
 
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bouboulecorp.thurii.R
 
@@ -8,5 +10,43 @@ class DoubleAuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_double_auth)
+
+        val back_btn = findViewById<ImageButton>(R.id.backbtn)
+
+        back_btn.setOnClickListener {
+            super.onBackPressed();
+        }
+
+        val TelAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.country_tel_code,
+            R.layout.spinner_item
+        )
+        val tels = findViewById<Spinner>(R.id.tel_spinner)
+        tel_listener(TelAdapter, tels)
+    }
+
+    fun tel_listener(TelAdapater:ArrayAdapter<CharSequence>, tels:Spinner) {
+        TelAdapater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tels.adapter = TelAdapater
+        tels.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position != 0) {
+                    val selectedItem = parent!!.getItemAtPosition(position)
+                    Toast.makeText(
+                        applicationContext,
+                        "$selectedItem Selected",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
     }
 }
--- app/src/main/java/com/bouboulecorp/thurii/EmailRegistration/RegisterEmailActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/EmailRegistration/RegisterEmailActivity.kt
@@ -1,10 +1,13 @@
 package com.bouboulecorp.thurii.EmailRegistration
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import com.bouboulecorp.thurii.DoubleAuth.DoubleAuthActivity
 import com.bouboulecorp.thurii.R
 
 class RegisterEmailActivity : AppCompatActivity() {
@@ -13,6 +16,16 @@ class RegisterEmailActivity : AppCompatActivity() {
         setContentView(R.layout.registration_email)
 
         val checked = findViewById<ImageView>(R.id.checked)
+        val continue_btn = findViewById<Button>(R.id.continueBtn)
+        val back_btn = findViewById<ImageButton>(R.id.backbtn)
+
+        back_btn.setOnClickListener {
+            super.onBackPressed();
+        }
         checked.visibility = View.INVISIBLE
+        continue_btn.setOnClickListener {
+            val intent = Intent(this, DoubleAuthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
