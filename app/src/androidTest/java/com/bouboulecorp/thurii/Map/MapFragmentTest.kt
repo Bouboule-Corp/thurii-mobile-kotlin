9f54cfdc378d9134ee2186a314d924d4a14dba43
Neyross <74605059+Neyross@users.noreply.github.com>
2022-06-13 23:37:52 +0200

    base map with hidden key

---
+++ app/src/androidTest/java/com/bouboulecorp/thurii/Map/MapFragmentTest.kt
@@ -0,0 +1,11 @@
+package com.bouboulecorp.thurii.Map
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+
+class MapFragmentTest {
+    @Test
+}
