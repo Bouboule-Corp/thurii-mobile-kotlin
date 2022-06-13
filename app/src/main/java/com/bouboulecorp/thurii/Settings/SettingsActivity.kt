ec759a7961cc932ebe6b0d08bef0ad3e014adbc0
Lucas Arroyo <75220024+larroyo3@users.noreply.github.com>
2022-06-14 01:21:44 +0200

    Update SettingsActivity.kt

--- app/src/main/java/com/bouboulecorp/thurii/Settings/SettingsActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/Settings/SettingsActivity.kt
@@ -21,13 +21,13 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun changeToFrench(view: View) {
-        LocaleHelper.setLocale(this,"fr") //for french;
+        LocaleHelper.setLocale(this, "fr") //for french;
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
     fun changeToEnglish(view: View) {
-        LocaleHelper.setLocale(this,"en") //for english;
+        LocaleHelper.setLocale(this, "en") //for english;
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
