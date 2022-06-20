64576eee28550e32304fbea01cc1e7d40ca38ba3
Neyross <74605059+Neyross@users.noreply.github.com>
2022-06-21 00:08:46 +0200

    now get api key from env variables

--- app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/MapFragment.kt
+++ app/src/main/java/com/bouboulecorp/thurii/HomePage/HomePageFragments/MapFragment.kt
@@ -4,29 +4,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bouboulecorp.thurii.BuildConfig
 import com.bouboulecorp.thurii.R
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.maps.Settings.get
 
 class MapFragment : Fragment() {
 
     private var mapView: MapView? = null
-    private var key: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        if (getString(R.string.mapbox_access_token) != null)
-            key = getString(R.string.mapbox_access_token)
-        else
-            key = System.getenv("MAPBOX_KEY")
-        Mapbox.getInstance(requireActivity(), key)
-
+        Mapbox.getInstance(requireActivity(), getString(R.string.mapbox_access_token))
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
 
         mapView = view.findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
