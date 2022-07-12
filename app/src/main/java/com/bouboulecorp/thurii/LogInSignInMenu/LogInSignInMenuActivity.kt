2df23c97b4c6561da8b98e5e599ff838aeb90eba
lucas <lucas.arroyo70@gmail.com>
2022-07-13 01:26:11 +0200

    add theme handling

--- app/src/main/java/com/bouboulecorp/thurii/LogInSignInMenu/LogInSignInMenuActivity.kt
+++ app/src/main/java/com/bouboulecorp/thurii/LogInSignInMenu/LogInSignInMenuActivity.kt
@@ -1,11 +1,14 @@
 package com.bouboulecorp.thurii.LogInSignInMenu
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.bouboulecorp.thurii.R
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.bouboulecorp.thurii.LogIn.LogInActivity
 import com.bouboulecorp.thurii.Registration.RegistrationFragment.RegistrationActivity
 
@@ -22,6 +25,22 @@ class LogInSignInMenuActivity : AppCompatActivity() {
         // Finding and displaying the content
         // that consists a URL as a hyperlink
         mTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        val sharedPreference = getSharedPreferences("USER_VARIABLES", Context.MODE_PRIVATE)
+        when (sharedPreference.getInt("theme_selected", 2)) {
+            0 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+            }
+            1 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            }
+            2 -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                delegate.applyDayNight()
+            }
+        }
     }
 
     fun goToLogInPage(view: View) {
